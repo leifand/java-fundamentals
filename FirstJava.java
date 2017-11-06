6e00bd321495c5b44b7aaf00a06949c071917919
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-06 07:12:37 -0600

    FirstJava class

---
+++ FirstJava.java
@@ -0,0 +1,12 @@
+/*
+  FirtJava.java
+  Leif Anderson, November 6th, 2017
+*/
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Leif Anderson.");
+        System.out.println("I am 51 years old.");
+        System.out.println("My hometown is Dallas, Texas.");
+    }
+}
