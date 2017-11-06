a9e244f4e0aa58b691f13378da8593ac3cc48d5e
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-06 07:40:25 -0600

    Pythagorean example

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean pTheorem = new Pythagorean();
+        double result = pTheorem.calculateHypotenuse(3, 4);
+        System.out.println("The hypotenuse is " + result);
+    }
+}
