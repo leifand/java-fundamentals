312b13158551a37024297274a0f514d9463f1a41
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-13 15:57:20 -0600

    Hashmatique - need to fix printSongs

---
+++ Hashmatique.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class Hashmatique {
+
+  private static HashMap<String, String> trackList = new HashMap<String, String>();
+  public static void main(String[] args) {
+
+    Hashmatique my_hash = new Hashmatique();
+    my_hash.addSongs();
+    my_hash.printSong();
+    my_hash.printSongs();
+  }
+
+  public void addSongs() {
+    trackList.put("Rockstar", "Im done with standing in line at clubs Ill never get in.");
+    trackList.put("Control", "I love the way you look at me.");
+    trackList.put("Behind Blue Eyes", "Noone knows what its like to be the bad man.");
+    trackList.put("Black Sun", "Theres whiskey in the water, and there is death upon the vine.");
+  }
+
+  public void printSong() {
+    System.out.println(trackList.get("Control"));
+  }
+
+  public void printSongs() {
+    for (Object song : trackList) {
+      System.out.println(song.getKey() + " : " + song.getValue());
+    }
+  }
+}
