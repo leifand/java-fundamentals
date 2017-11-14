07a45357d321f19e15851f63422b7946dbd2254f
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-13 21:23:28 -0600

    ExceptionallyGeneric

---
+++ ExceptionallyGeneric.java
@@ -0,0 +1,16 @@
+public class ExceptionallyGeneric {
+
+  public static void main(String[] args) {
+
+    ArrayList<Object> myList = new ArrayList<Object>();
+
+    myList.add("13");
+    myList.add("hello world");
+    myList.add(48);
+    myList.add("Goodbye World");
+
+    try {
+      for
+    } catch (ClassCastException e) {
+        System.out.println("ERROR ON INDEX {#}");
+    }
